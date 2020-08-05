960bba7e053181d8ba4acd8388cbf9bbe14a90f5
Xolesteroll <xol2dem@gmail.com>
2020-08-05 15:16:51 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log('arr' + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 != 8);
+
+const isCheked = false,
+      isClosed = false;
+
+console.log(isCheked || !isClosed);
