4e4aa3156396ad03e614cbbcf734bf2834e83c78
Xolesteroll <xol2dem@gmail.com>
2020-08-06 16:08:08 +0300

    условия, циклы, функции, методы и свойства

--- js/practice.js
+++ js/practice.js
@@ -22,15 +22,15 @@
 
 // Использование циклов и условий часть 2.
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
 // const a = prompt('Один из последних просмотренных фильмов', ''),
 //       b = prompt('На сколько оцените его?', ''),
@@ -40,13 +40,118 @@ const personalMovieDB = {
 // personalMovieDB.movies[a] = b;
 // personalMovieDB.movies[c] = d;
 
-for (let i = 0; i < 2; i++) { 
-    const a = prompt('Один из последних просмотренных фильмов', ''),
-          b = prompt('На сколько оцените его?', '');
+// for (let i = 0; i < 2; i++) { 
+//     const a = prompt('Один из последних просмотренных фильмов', ''),
+//           b = prompt('На сколько оцените его?', '');
 
-    personalMovieDB.movies[a] = b;
+//     if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('Error');
+//         i--;
+//     }
+    
+// }
+
+
+// if (personalMovieDB.count < 10){
+//     console.log('ВЫсмотрели слишком мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+//     console.log('Вы ахуенный кинокритик, почти бэдкомедианТ');
+// } else if (personalMovieDB.count >= 30){
+//     console.log('Удачи вам по жизни, фарту масти');
+// } else {
+//     console.log('Произошла ошибка');
+// }
+
+// console.log(personalMovieDB);
+
+
+//использование функций часть 3.
+
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
 }
 
-console.log(personalMovieDB);
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) { 
+        const a = prompt('Один из последних просмотренных фильмов', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('Error');
+            i--;
+        }
+        
+    }
+}
+
+rememberMyFilms();
+
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10){
+        console.log('ВЫсмотрели слишком мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log('Вы ахуенный кинокритик, почти бэдкомедианТ');
+    } else if (personalMovieDB.count >= 30){
+        console.log('Удачи вам по жизни, фарту масти');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+detectPersonalLevel();
+// Мой вариант
+// function showMyDB() {
+//     (personalMovieDB.privat == false) ? console.log(personalMovieDB) : console.log("No access");
+// }
+// showMyDB();
+
+//Вариант учителя
+
+
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++){
+        // const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        // personalMovieDB.genres[i - 1] = genre;
+        //Укороченный вариант
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
 
 
--- js/script.js
+++ js/script.js
@@ -5,65 +5,152 @@
 // Условия if / else
 
 
-if (4 == 9) {
-    console.log('OK!');
-} else {
-    console.log('Error!');
-}
-
-
-
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Много');
+// if (4 == 9) {
+//     console.log('OK!');
 // } else {
-//     console.log('Ok!');
+//     console.log('Error!');
 // }
 
-// (num == 50) ? console.log('Ok!') : console.log('Error');
-
-// Оператор switch как замена if / else работает только с точным сравнением по значению, тоесть не может
-// проверять условия больше или меньше, только на предмет равенства.
 
 
+// // if (num < 49) {
+// //     console.log('Error');
+// // } else if (num > 100) {
+// //     console.log('Много');
+// // } else {
+// //     console.log('Ok!');
+// // }
 
-switch (50) {
-    case 49:
-        console.log('Неверно');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    case 50:
-        console.log('Верно');
-        break;
-    default:
-        console.log('Не в этот раз');
-        break;
-}
+// // (num == 50) ? console.log('Ok!') : console.log('Error');
 
-// Циклы
+// // Оператор switch как замена if / else работает только с точным сравнением по значению, тоесть не может
+// // проверять условия больше или меньше, только на предмет равенства.
 
-let num = '50';
 
-// while (num <= 55) {
+
+// switch (50) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case 50:
+//         console.log('Верно');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
+
+// // Циклы
+
+// // let num = '50';
+
+// // while (num <= 55) {
+// //     console.log(num);
+// //     num++;
+// // }
+
+// // do {
+// //     console.log(num);
+// //     num++;
+// // }
+// // while(num < 55);
+
+// // for (let i = 0; i <= 10; i++){
+// //     console.log(num);
+// //     num++;
+// //     if (i === 6 || num === 56){
+// //         // break;
+// //         continue;
+// //     }
+// //     // console.log(num);
+// //     // num++;
+// //     console.log(i);
+// // }
+
+// // ФУНКЦИИ
+
+// let num = 20;
+
+// function showFirstMEssage(text) {
+//     console.log(text);
+//     // let num = 10;
 //     console.log(num);
-//     num++;
 // }
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while(num < 55);
+// showFirstMEssage("ИДИ нАх УЙЙЙЙ");
+// console.log(num);
 
-for (let i = 1; i <= 10; i++){
-    if (i === 6){
-        // break;
-        continue;
-    }
-    // console.log(num);
-    // num++;
-    console.log(i);
-}
+// // function calc(a, b) {
+// //     return (a + b);
+// //     // console.log('dsad')      Недоступный код после return(unreachable)
+// // }
+
+// // console.log(calc(4, 3));
+// // console.log(calc(5, 6));
+// // console.log(calc(109, 212));
+// // console.log(calc(45, 32131));
+
+// function ret() {
+//     let num = 50;
+//     return num + 5;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function() {
+//     console.log('Hello');
+// };
+
+// logger();
+
+// const calc = (a, b) => {
+//     console.log('1');
+//     return a + b;
+// };
+
+// console.log(calc(5, 10));
+
+// МЕТОДЫ И СВОЙСТВА СТРОК И ЧИСЕЛ
+
+const str = "teSt";
+const arr = [1, 2, 4];
+
+// console.log(str[2] = "d");
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("q"));  // Поиск в строке
+
+const logg = "Убись Конем Кончужка";  
+// Вырезаем слова из строк по индексам, аргументы - начало отрезка и конец отрезка
+// не включительно, данный метод поддерживает отрицательные индексы, в таком случае
+// отсчет начинается с конца стрроки
+console.log(logg.slice(0, 5) + " " + logg.slice(12) + " " + logg.slice(6, 11));
+// ТОт же метод, только в профиль. НЕ поддерживает отрицательные индексы и
+// допускает вариант когда первый аргумент больше чем второй
+console.log(logg.substring(11, 6));
+// Похожий метод, разница в том что первый аргумент указывает начало отрезка строки,
+// А второй аргумент указывает количества символом после него которые надо вырезать
+console.log(logg.substr(6, 5));
+
+
+// Методы для чисел
+
+// данный метод округляет число до ближайшего целого
+const num = 12.2;
+console.log(Math.round(num));
+// данный метод трансформирует строку в число. в данном случае обрезает строку до числа до десятичной запятой.
+const test = "12.3px";
+console.log(parseInt(test));
+//данный метод трансформирует строку в число не обрезая число до десятичной запятой.
+console.log(parseFloat(test));
+
+
+
