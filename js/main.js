8726a58f9a92f11d5fdee457da81fd49eaf151e8
Xolesteroll <xol2dem@gmail.com>
2020-08-11 18:13:39 +0300

    Основы ООП, Практика, Дебаггер в консоли браузера, Динамическая типизация в JS

--- js/main.js
+++ js/main.js
@@ -1,22 +1,58 @@
 "use strict";
 
-console.log('arr' + ' - object');
+// console.log('arr' + ' - object');
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-// ++incr;
-// --decr;
+// // ++incr;
+// // --decr;
 
-console.log(++incr);
-console.log(--decr);
+// console.log(++incr);
+// console.log(--decr);
 
 
-console.log(5%2);
+// console.log(5%2);
 
-console.log(2 + 2 * 2 != 8);
+// console.log(2 + 2 * 2 != 8);
 
-const isCheked = false,
-      isClosed = false;
+// const isCheked = false,
+//       isClosed = false;
 
-console.log(isCheked || !isClosed);
+// console.log(isCheked || !isClosed);
+
+// const str = prompt("", "");
+// const products = str.split(", "); // Получение массива из отдельных элементов через запятую
+// products.sort(); //Сортировка элементов внутри массива, 
+// console.log(products.join("! ")); // Предоставление массива ввиде строки с разделителем "!" в данном случае
+
+// let str = "some",
+//     strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1, 2, 3]);
+
+
+//ПРототипное наследование
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function(){
+        console.log("Hello");
+    }
+};
+
+const jonh = Object.create(soldier); //Прототипное наследование во время создания объекта
+
+// const jonh = {
+//     health: 100
+// };
+
+// // jonh.__proto__ = soldier; //Устаревший метод
+
+// Object.setPrototypeOf(jonh, soldier); // БОлее новый способ
+
+console.log(jonh.armor);
+jonh.sayHello(); //ФУнкци тоже наследуются по прототипному наследованию
