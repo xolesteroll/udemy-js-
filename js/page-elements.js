3ae3b263f50ddfe12a70cd2cfee57db5be346d27
xolesteroll <xol2dem@gmail.com>
2020-08-13 17:49:43 +0300

    merge

--- js/page-elements.js
+++ js/page-elements.js
@@ -1,28 +1,40 @@
 "use strict";
 // порядком устаревшие методы
-const box = document.getElementById("box");
+const box = document.getElementById("box"),
 
-console.log(box);
+// console.log(box);
 
-const buttons = document.getElementsByTagName("button");
+    buttons = document.getElementsByTagName("button"),
 
-console.log(buttons[1]);
+// console.log(buttons[1]);
 
-const circles = document.getElementsByClassName("circle");
+    circles = document.getElementsByClassName("circle"),
 
-console.log(circles);
+// console.log(circles);
 
 // Методы поновее
 
-const hearts = document.querySelectorAll(".heart");
+    hearts = document.querySelectorAll(".heart"),
 
-hearts.forEach(item => {
-    console.log(item);
-});
+// hearts.forEach(item => {
+//     console.log(item);
+// });
 
 
-const oneHeart = document.querySelector(".heart"); // возвращает первый попавшийся элемент под указанным селектором
+    oneHeart = document.querySelector(".heart"); // возвращает первый попавшийся элемент под указанным селектором
 
-console.log(oneHeart);
+// console.log(oneHeart);
 
+console.dir(box);// Элемент на странице воспринимается как объект с помозью назначения переменной сожержащую сам этот элемент
+
+
+
+// box.style.backgroundColor = "blue";  // Или так
+// box.style.width = "500px";
+
+box.style.cssText = "background-color: blue; width: 500px;";  // Или так
+box.style.cssText = `background-color: blue; width: ${num}px;`;  // Или так
+
+buttons[1].style.borderRadius = "100%";
+circles[0].style.backgroundColor = "red";
 
