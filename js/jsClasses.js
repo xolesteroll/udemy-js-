96a1847e5bb85d7a4948dd7b2bd32f3cb98443b3
Xolesteroll <xol2dem@gmail.com>
2020-08-20 19:31:29 +0300

    классы в javascript

---
+++ js/jsClasses.js
@@ -0,0 +1,35 @@
+'use strict';
+
+class Rectangle {
+    constructor(height, width){
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.width * this.height;
+    }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps(){
+        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Hello', 'red');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
